4830e358c7012dd905cae8b9ef985c0981c8244a
creatorio <rdjunior018@gmail.com>
2026-03-24 17:59:33 +0530

    Fixed SMTP Issue

--- smtp.js
+++ smtp.js
@@ -1,7 +1,9 @@
 ﻿const nodemailer = require("nodemailer");
 const send = (mailOptions) => {
   var transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: "rent.manager.corp@gmail.com",
       pass: "ljll myiy mube itmh",
